a4a47ea9238113e6c76255504ffe29e9c05bc038
FathirRD <fathirrahmand@gmail.com>
2021-05-05 20:38:10 +0700

    Again try fix host

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: process.env.NODE_ENV !== 'production' ? 5000 : process.env.PORT || 3000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : process.env.HOST || '0.0.0.0',
     routes: {
       cors: {
         origin: ['http://ec2-13-212-153-62.ap-southeast-1.compute.amazonaws.com:8000'],

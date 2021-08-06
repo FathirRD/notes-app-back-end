1ab2726f9c9257bddcd9855b2267911a5c5dc075
FathirRD <fathirrahmand@gmail.com>
2021-08-06 20:12:08 +0700

    Add pg db and implement pg services

--- src/server.js
+++ src/server.js
@@ -1,16 +1,21 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
-const NotesService = require('./services/inMemory/NotesService');
+// const NotesService = require('./services/inMemory/NotesService');
+const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: process.env.NODE_ENV !== 'production' ? 5000 : process.env.PORT || 3000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : process.env.HOST || '0.0.0.0',
+    // port: process.env.NODE_ENV !== 'production' ? 5000 : process.env.PORT || 3000,
+    // host: process.env.NODE_ENV !== 'production' ? 'localhost' : process.env.HOST || '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
-        origin: ['http://ec2-13-212-153-62.ap-southeast-1.compute.amazonaws.com:8000'],
+        origin: ['http://notesapp-v1.dicodingacademy.com'],
       },
     },
   });
